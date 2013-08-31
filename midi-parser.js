a9fc90d588185e2f69596f81d440c0c6dbd0d9ef
Francis Gulotta <wizard@roborooter.com>
2013-08-31 17:49:26 -0400

    Midi Parser and some basic tests

--- midi-parser.js
+++ midi-parser.js
@@ -1,9 +1,41 @@
-var buffer = [];
+var util = require("util");
+var events = require('events');
 
 var Parser = module.exports = function () {
-
+  events.EventEmitter.call(this);
+  this.buffer = [];
 };
 
-Parser.prototype.buffer = function () {
-  return buffer;
+util.inherits(Parser, events.EventEmitter);
+
+var msg = Parser.msg = {
+  START_SYSEX: 240, // 0xF0,
+  END_SYSEX: 247 //0xF7
+};
+
+Parser.prototype.write = function (data) {
+  for (var i = 0; i < data.length; i++) {
+    var byt = data[i];
+    this.writeByte(byt);
+  }
+};
+
+Parser.prototype.writeByte = function (byt) {
+  this.buffer.push(byt);
+
+  var last = this.buffer.length - 1;
+  var starts_sysx = this.buffer[0] === msg.START_SYSEX;
+  var ends_sysx = this.buffer[last] === msg.END_SYSEX;
+
+  if (starts_sysx && ends_sysx) {
+    this.emit('sysx', this.buffer.slice(1, -1));
+    this.buffer.length = 0;
+    return;
+  }
+
+  if (!starts_sysx && this.buffer.length === 3) {
+    this.emit('midi', this.buffer.slice());
+    this.buffer.length = 0;
+  }
+
 };
--- test/midi-parser.js
+++ test/midi-parser.js
@@ -1,15 +1,48 @@
 var Parser = require('../midi-parser');
 
+var msg = Parser.msg;
+
 module.exports.setUp = function (cb) {
   this.parser = new Parser();
   cb();
 };
 
-module.exports.writeMessage = function (test) {
-  test.done();
+module.exports.writeSysx = function (test) {
+  test.expect(1);
+  var message = [Parser.msg.START_SYSEX, 99, Parser.msg.END_SYSEX];
+  this.parser.on('sysx', function (data) {
+    test.deepEqual(data, [99] );
+    test.done();
+  });
+  this.parser.write(message);
 };
 
-module.exports.buffer = function (test) {
-  test.deepEqual(this.parser.buffer(), []);
+module.exports.writeMidi = function (test) {
+  test.expect(1);
+  var message = [1, 99, 3];
+  this.parser.on('midi', function (data) {
+    test.deepEqual(data, message);
+    test.done();
+  });
+  this.parser.write(message);
+};
+
+module.exports.writeMultiple = function (test) {
+  test.expect(2);
+  var midiCmd = [1, 2, 3];
+  var sysxCmd = [55];
+
+  this.parser.on('midi', function (data) {
+    test.deepEqual(data, midiCmd);
+  });
+
+  this.parser.on('sysx', function (data) {
+    test.deepEqual(data, sysxCmd);
+
+  });
+
+  var sysxMsg = [Parser.msg.START_SYSEX].concat(sysxCmd).concat([Parser.msg.END_SYSEX]);
+  var message = midiCmd.concat(sysxMsg);
+  this.parser.write(message);
   test.done();
 };
