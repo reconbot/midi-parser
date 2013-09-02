b04709d8863890ba4b0f540f88194db227a5e4ff
Francis Gulotta <wizard@roborooter.com>
2013-09-02 02:12:51 -0400

    Not good enough for firmata yet.

---
+++ test/firmata-integration.js
@@ -0,0 +1,15 @@
+var FirmataPi = require('../firmata-pi');
+var Board = require('firmata').Board;
+var sinon = require('sinon');
+
+module.exports['Works with Firmata Board'] = function (test) {
+  // var firmataPi = new FirmataPi();
+  // // test.expect(1);
+
+  // var board = new Board(firmataPi, function (error) {
+  //   test.equal(error, undefined, 'Firmata started without erorr');
+  //   // keepTesting();
+  // });
+
+  test.done();
+};
