293d3b4ee1fb596c855bfbfef92b14749891e2bf
Francis Gulotta <wizard@roborooter.com>
2013-09-04 22:29:31 -0400

    Remove unnecessary stuff from the tests

--- test/parser.js
+++ test/parser.js
@@ -14,7 +14,6 @@ var describe = module.exports;
 
 describe["#emitMidi"] = function (test) {
   var spy = sinon.spy();
-  test.expect(1);
   this.parser.on('midi', spy);
   this.parser.emitMidi(soundOff);
   var called = spy.calledWith(176, 12, [120, 0]);
@@ -44,7 +43,6 @@ describe["sysex command"] = function (test) {
 describe["sysex command during command"] = function (test) {
   var message = [msg.startSysex, msg.startSysex, 99, msg.endSysex];
   var spy = sinon.spy();
-  test.expect(1);
   this.parser.on('sysex', spy);
   this.parser.write(message);
   test.ok(spy.calledWith(99, []), "sysex command emitted");
@@ -53,7 +51,6 @@ describe["sysex command during command"] = function (test) {
 
 describe["midi command"] = function (test) {
   var spy = sinon.spy();
-  test.expect(1);
   this.parser.on('midi', spy);
   this.parser.write(soundOff);
   test.ok(spy.calledWith(176, 12, [120, 0]), "midi command emitted");
@@ -62,7 +59,6 @@ describe["midi command"] = function (test) {
 
 describe["Command during a Sysex Command clears current Sysex"] = function (test) {
   var spy = sinon.spy();
-  test.expect(1);
   this.parser.on('midi', spy);
   this.parser.write([msg.startSysex]);
   this.parser.write(soundOff);
@@ -72,7 +68,6 @@ describe["Command during a Sysex Command clears current Sysex"] = function (test
 
 describe["midi System Realtime Commands emit immediately"] = function (test) {
   var spy = sinon.spy();
-  test.expect(1);
   this.parser.on('midi', spy);
   this.parser.write(systemReset);
   test.ok(spy.calledWith(systemReset[0], null, []), 'realtime command emited');
@@ -81,7 +76,6 @@ describe["midi System Realtime Commands emit immediately"] = function (test) {
 
 describe["midi System Realtime Commands emit during sysex"] = function (test) {
   var spy = sinon.spy();
-  test.expect(2);
   this.parser.on('midi', spy);
   this.parser.on('sysex', spy);
   this.parser.write([msg.startSysex, systemReset[0], 99, msg.endSysex]);
@@ -93,7 +87,6 @@ describe["midi System Realtime Commands emit during sysex"] = function (test) {
 describe["midi System Realtime Commands emit during midi channel"] = function (test) {
   var spy = sinon.spy();
   var mixed = [ 188, 0xFF, 120, 0 ];
-  test.expect(2);
   this.parser.on('midi', spy);
   this.parser.write(mixed);
   test.ok(spy.getCall(0).calledWith(systemReset[0], null, []), 'realtime command emited');
@@ -103,7 +96,6 @@ describe["midi System Realtime Commands emit during midi channel"] = function (t
 
 describe["midi single byte commands"] = function (test) {
   var spy = sinon.spy();
-  test.expect(1);
   this.parser.on('midi', spy);
   this.parser.write([msg.tuneReq]); // [0xF6]
   test.ok(spy.calledWith(msg.tuneReq, null, []), "Single byte command emitted");
@@ -114,7 +106,6 @@ describe["midi channel voice messages"] = function (test) {
   var noteOnChan2 = msg.noteOn + 2;
   var packet = [noteOnChan2, 0, 0];
   var spy = sinon.spy();
-  test.expect(1);
   this.parser.on('midi', spy);
   this.parser.write(packet);
   test.ok(spy.calledWith(msg.noteOn, 2, [0, 0]), "Channel voice message");
@@ -125,7 +116,6 @@ describe["midi channel voice messages with 1 data byte"] = function (test) {
   var chanPressure2 = msg.chanPressure + 2;
   var packet = [chanPressure2, 0];
   var spy = sinon.spy();
-  test.expect(1);
   this.parser.on('midi', spy);
   this.parser.write(packet);
   test.ok(spy.calledWith(msg.chanPressure, 2, [0]), "Channel voice message");
